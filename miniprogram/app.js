1971b6231469ed9c5356d00dff40b1e7721cc32a
邹雨蒙 <yumengzou2@gmail.com>
2026-04-21 19:56:24 +0800

    fix: update serverUrl to correct Railway deployment URL

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,6 +1,6 @@
 App({
   globalData: {
-    serverUrl: 'https://pleasant-charisma-production-8c7e.up.railway.app',
+    serverUrl: 'https://responsible-imagination-production-a861.up.railway.app',
     roomId: null,
     playerId: null,
     playerName: null,
